e91304b80497636a5e37bef292314b33b957392e
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-11 21:43:54 +0200

    finished

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,21 @@
 import Alpine from 'alpinejs'
 
+
 window.Alpine = Alpine
 
-Alpine.start()
+Alpine.start();
+
+// init Swiper:
+const swiper = new Swiper('.work-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 15,
+    centeredSlides: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
